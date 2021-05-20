37922e8a0ead6ffa7e932fe474c1c29564209bf8
prabhat-cn <prabhat@capitalnumbers.com>
2021-05-20 12:37:57 +0530

    🚀

--- pages/components/Footer.jsx
+++ pages/components/Footer.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 export default function Footer () {
     return (
     <footer>
-        <div class="container">
-        <div class="row">
-            <div class="col-lg-12 wow fadeIn" data-wow-duration="1s" data-wow-delay="0.25s">
+        <div className="container">
+        <div className="row">
+            <div className="col-lg-12 wow fadeIn" data-wow-duration="1s" data-wow-delay="0.25s">
             <p>© Copyright 2021 Space Dynamic Co. All Rights Reserved. 
             
             <br />Design: <a rel="nofollow" href="https://templatemo.com">TemplateMo</a></p>
--- pages/layouts/About.jsx
+++ pages/layouts/About.jsx
@@ -2,56 +2,56 @@ import React from 'react'
 
 const About = () => {
     return (
-        <div id="about" class="about-us section">
-    <div class="container">
-      <div class="row">
-        <div class="col-lg-4">
-          <div class="left-image wow fadeIn" data-wow-duration="1s" data-wow-delay="0.2s">
+        <div id="about" className="about-us section">
+    <div className="container">
+      <div className="row">
+        <div className="col-lg-4">
+          <div className="left-image wow fadeIn" data-wow-duration="1s" data-wow-delay="0.2s">
             <img src="assets/images/about-left-image.png" alt="person graphic" />
           </div>
         </div>
-        <div class="col-lg-8 align-self-center">
-          <div class="services">
-            <div class="row">
-              <div class="col-lg-6">
-                <div class="item wow fadeIn" data-wow-duration="1s" data-wow-delay="0.5s">
-                  <div class="icon">
+        <div className="col-lg-8 align-self-center">
+          <div className="services">
+            <div className="row">
+              <div className="col-lg-6">
+                <div className="item wow fadeIn" data-wow-duration="1s" data-wow-delay="0.5s">
+                  <div className="icon">
                     <img src="assets/images/service-icon-01.png" alt="reporting" />
                   </div>
-                  <div class="right-text">
+                  <div className="right-text">
                     <h4>Data Analysis</h4>
                     <p>Lorem ipsum dolor sit amet, ctetur aoi adipiscing eliter</p>
                   </div>
                 </div>
               </div>
-              <div class="col-lg-6">
-                <div class="item wow fadeIn" data-wow-duration="1s" data-wow-delay="0.7s">
-                  <div class="icon">
+              <div className="col-lg-6">
+                <div className="item wow fadeIn" data-wow-duration="1s" data-wow-delay="0.7s">
+                  <div className="icon">
                     <img src="assets/images/service-icon-02.png" alt="" />
                   </div>
-                  <div class="right-text">
+                  <div className="right-text">
                     <h4>Data Reporting</h4>
                     <p>Lorem ipsum dolor sit amet, ctetur aoi adipiscing eliter</p>
                   </div>
                 </div>
               </div>
-              <div class="col-lg-6">
-                <div class="item wow fadeIn" data-wow-duration="1s" data-wow-delay="0.9s">
-                  <div class="icon">
+              <div className="col-lg-6">
+                <div className="item wow fadeIn" data-wow-duration="1s" data-wow-delay="0.9s">
+                  <div className="icon">
                     <img src="assets/images/service-icon-03.png" alt="" />
                   </div>
-                  <div class="right-text">
+                  <div className="right-text">
                     <h4>Web Analytics</h4>
                     <p>Lorem ipsum dolor sit amet, ctetur aoi adipiscing eliter</p>
                   </div>
                 </div>
               </div>
-              <div class="col-lg-6">
-                <div class="item wow fadeIn" data-wow-duration="1s" data-wow-delay="1.1s">
-                  <div class="icon">
+              <div className="col-lg-6">
+                <div className="item wow fadeIn" data-wow-duration="1s" data-wow-delay="1.1s">
+                  <div className="icon">
                     <img src="assets/images/service-icon-04.png" alt="" />
                   </div>
-                  <div class="right-text">
+                  <div className="right-text">
                     <h4>SEO Suggestions</h4>
                     <p>Lorem ipsum dolor sit amet, ctetur aoi adipiscing eliter</p>
                   </div>
