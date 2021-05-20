c54b8b765db5f6fc8760d083dae36b9945877d2a
prabhat-cn <prabhat@capitalnumbers.com>
2021-05-20 13:07:53 +0530

    Contact indendation solved

--- pages/layouts/Contact.jsx
+++ pages/layouts/Contact.jsx
@@ -2,32 +2,34 @@ import React from 'react'
 
 const Contact = () => {
     return (
-        <div id="contact" className="contact-us section">
-    <div className="container">
-      <div className="row">
-        <div className="col-lg-6 align-self-center wow fadeInLeft" data-wow-duration="0.5s" data-wow-delay="0.25s">
-          <div className="section-heading">
-            <h2>Feel Free To Send Us a Message About Your Website Needs</h2>
+    <div id="contact" className="contact-us section">
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-6 align-self-center wow fadeInLeft" data-wow-duration="0.5s" data-wow-delay="0.25s">
+            <div className="section-heading">
+             <h2>Feel Free To Send Us a Message About Your Website Needs</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed doer ket eismod tempor incididunt ut labore et dolores</p>
-            <div className="phone-info">
-              <h4>For any enquiry, Call Us: <span><i className="fa fa-phone"></i> <a href="#">010-020-0340</a></span></h4>
+             <div className="phone-info">
+              <h4>For any enquiry, Call Us: <span><i className="fa fa-phone"></i> 
+              <a href="#">010-020-0340</a></span>
+              </h4>
+            </div>
             </div>
           </div>
-        </div>
-        <div className="col-lg-6 wow fadeInRight" data-wow-duration="0.5s" data-wow-delay="0.25s">
-          <form id="contact" action="" method="post">
+          <div className="col-lg-6 wow fadeInRight" data-wow-duration="0.5s" data-wow-delay="0.25s">
+            <form id="contact" action="" method="post">
             <div className="row">
-              <div className="col-lg-6">
+                <div className="col-lg-6">
                 <fieldset>
                   <input type="name" name="name" id="name" placeholder="Name" autoComplete="on" required />
                 </fieldset>
               </div>
-              <div className="col-lg-6">
+                <div className="col-lg-6">
                 <fieldset>
                   <input type="surname" name="surname" id="surname" placeholder="Surname" autoComplete="on" required />
                 </fieldset>
               </div>
-              <div className="col-lg-12">
+                <div className="col-lg-12">
                 <fieldset>
                   <input type="text" name="email" id="email" pattern="[^ @]*@[^ @]*" placeholder="Your Email" required="" />
                 </fieldset>
@@ -46,10 +48,10 @@ const Contact = () => {
             <div className="contact-dec">
               <img src="assets/images/contact-decoration.png" alt="" />
             </div>
-          </form>
+           </form>
+          </div>
         </div>
       </div>
-    </div>
   </div>
     )
 }
