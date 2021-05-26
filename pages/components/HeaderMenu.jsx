810495d0d9b8a0ac18402ec166f9f55873211114
prabhat-cn <prabhat@capitalnumbers.com>
2021-05-26 12:17:40 +0530

    issue solved

--- pages/components/HeaderMenu.jsx
+++ pages/components/HeaderMenu.jsx
@@ -11,14 +11,19 @@ const HeaderMenu = () => {
   }
 
 
-  // const linkActiveOnPageLoad = (id) => {
-  //   document.querySelectorAll('.scroll-to-section a').forEach(el => el.classList.remove('active'));
-  //   document.querySelector(`${id}-link`).classList.add('active');
-  // }
+  const linkActiveOnPageLoad = (id) => {
+    // console.log('id->', id);
+    document.querySelectorAll('.scroll-to-section a').forEach(el => el.classList.remove('active'));
+    document.querySelector(`${id}-link`).classList.add('active');
+  }
 
-  // useEffect(() => {
-  //   linkActiveOnPageLoad(window.location.hash);
-  // }, []);
+  useEffect(() => {
+    if(window.location.hash !== ''){
+      linkActiveOnPageLoad(window.location.hash);
+    }
+    
+    
+  }, []);
 
     return (
       <header className="header-area header-sticky wow slideInDown" data-wow-duration="0.75s" data-wow-delay="0s">
